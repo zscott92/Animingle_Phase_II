cc24fe783942e722cfadcf721ae04ca050424de2
Will West <willwest8xp@gmail.com>
2019-08-02 13:23:42 -0400

    sends follow-up queries

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,50 +23,108 @@ function getMatch(faceResponse) {
 
     let rand = Math.random() * 100;
 
-    const emotion = (function getEmotion (emotions) {
+    const emotion = (function getEmotion(emotions) {
         for (e in emotions) {
             rand -= emotions[e];
             if (rand <= 0) {
                 return e;
             }
         }
-    }) (emotions);
+    })(emotions);
 
     console.log(emotion);
 
     const key = transform[emotion];
-    
-    const queryString = "https://api.jikan.moe/v3/search/character/?" + $.param({q: key});
+
+    const queryString = "https://api.jikan.moe/v3/search/character/?" + $.param({ q: key });
 
     console.log(queryString);
 
-    $.get(queryString).then(
-        function(results) {
+    return $.get(queryString).then(
+        function (results) {
             //TODO filter results
             console.log(results);
+
+            const resultCount = results.results.length;
+            let sample = getRandom(results.results, resultCount > 9 ? 10 : resultCount);
+            console.log(sample);
+
+
+
+            return Promise.all(
+
+                // sample.map((item, index) => {
+                //     return new Promise(resolve => setTimeout(resolve, 550 * index)).then(
+                //         () => $.get(`https://api.jikan.moe/v3/character/${item.mal_id}`)
+                //     )
+                // })
+
+                sample.map(async (item, index) => {
+                    await new Promise(resolve => setTimeout(resolve, 550 * index));
+                    return $.get(`https://api.jikan.moe/v3/character/${item.mal_id}`);
+                })
+
+                // (function requestNext(iter, accumulator) {
+                //     const next = iter.next();
+                //     if (next) {
+                //         return iter.map( (item, index) => {
+                //             return new Promise(resolve =>  setTimeout( resolve , 550 * index)).then(
+                //                 () => $.get(`https://api.jikan.moe/v3/character/${item.mal_id}`)
+                //             )
+                //         })
+
+                //         // iter.map( async (item, index) => {
+                //         //     await new Promise(resolve => setTimeout(resolve, 550 * index));
+                //         //     return $.get(`https://api.jikan.moe/v3/character/${item.mal_id}`);
+                //         // })
+
+                //     } else {
+                //         return accumulator;
+                //     }
+                // })(sample, [])
+
+            );
         }
     )
 
-    // Return a promise
+
 }
 
-getMatch( {
-    faces:  [
+// https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array
+function getRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
+
+getMatch({
+    faces: [
         {
             attributes: {
                 "emotions": {
-                        "sadness": 0.378,
-                        "neutral": 75.48,
-                        "disgust": 0.004,
-                        "anger": 0.004,
-                        "surprise": 23.765,
-                        "fear": 0.004,
-                        "happiness": 0.363
-                      }
-                
+                    "sadness": 0.378,
+                    "neutral": 75.48,
+                    "disgust": 0.004,
+                    "anger": 0.004,
+                    "surprise": 23.765,
+                    "fear": 0.004,
+                    "happiness": 0.363
+                }
+
             }
         }
     ]
 
 }
+).then(
+    console.log
 )
