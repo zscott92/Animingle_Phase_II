088b16499e43237eb859d9eebf90fcbf51ce620b
Will <willwest8xp@gmail.com>
2019-08-05 00:13:03 -0400

    new branch

---
+++ assets/javascript/Profile.js
@@ -0,0 +1,41 @@
+class Profile {
+    constructor(obj) {
+        this.name = obj.name;
+        this.statsFull = obj.stats;
+        this.about = obj.about;
+        this.raw = obj.raw;
+        this.featured = obj.featured;
+        this.image_url = obj.image_url;
+    }
+
+    get bio() {
+        return this.about;
+    }
+
+    get stats() {
+        if (!this.statsFull) return false;
+
+        const stats = {};
+        if (this.statsFull.height) {
+            stats.height = this.height;
+        }
+        if (this.statsFull.weight) {
+            stats.weight = this.weight;
+        }
+        if (this.statsFull.personality) {
+            stats.personality = this.personality;
+        }
+
+        return stats
+    }
+
+    buildNode() {
+        return $('<div>').addClass('profile').append(
+            $('<div>').addClass('image profile-picture').append(
+                $('<img>').attr('src', this.image_url),
+                $('<h2>').addClass('profile-name').text(this.name),
+                $('<p>').text(this.bio),
+            )
+        );
+    }
+}
---
+++ assets/javascript/filters.js
@@ -0,0 +1,18 @@
+profileFilters = [
+    // filters take the mostly processed data we use to make profiles as input
+    function (p) { // filter out profiles that don't have a bio
+        console.log(p.about);
+        return p.about;
+    },
+    function (p) { // filter by gender
+        const match = p.about.match(/(?:^|\s)(he|she|him|her)(?:\s|$)/i);
+        return !(match && ['he','him'].includes(match[1].toLowerCase()))
+    },
+    // Content filters
+    function (p) { // age filter
+        return !(p.stats && p.stats.age && p.stats.age < 16);
+    },
+    function (p) {
+        return !p.raw.includes('incest');
+    },
+];
