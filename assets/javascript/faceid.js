2965b45dc3ebc9c577534314a8d6e67f3d046328
Kevin Abram <htwnkid@gmail.com>
2019-08-05 12:37:50 -0400

    version 2 of the faceid

--- assets/javascript/faceid.js
+++ assets/javascript/faceid.js
@@ -1,12 +1,16 @@
-function getFaceId() {
+$(document).ready(function() {
 
+function getFaceId(e, selectImgFile) {
+
+e.preventDefault();
     let data = new FormData();
     data.append("api_key", "ck3PwAKq4ZDsnbx77dyZG3lEk_YDwCIz");
     data.append("api_secret", "Epcw27lJerS2w28JQvd2DYhG_Rs-LjFJ");
-    data.append("image_url", "https://cdn.cnn.com/cnnnext/dam/assets/190802164147-03-trump-rally-0801-large-tease.jpg");
+    //data.append("image_url", "https://cdn.cnn.com/cnnnext/dam/assets/190802164147-03-trump-rally-0801-large-tease.jpg");
+    data.append("image_file", selectImgFile);
     data.append("return_attributes", "gender,age,smiling,headpose,emotion,ethnicity,mouthstatus,eyegaze");
 
-
+console.log(data);
     $.ajax({
         url: "https://api-us.faceplusplus.com/facepp/v3/detect",
         method: "POST",
@@ -15,7 +19,10 @@ function getFaceId() {
         processData: false,
         data: data
     }).then(function (response) {
-        console.log(response);
+        //console.log(response);
+
+        return(response);
+
     });
        
 }
@@ -24,5 +31,5 @@ function getFaceId() {
     
 $("#submitbtn").on("click", getFaceId);
 
-
+});
 
