506bd2241d80d512c8a95373344dc2baa6c1564f
zscott92 <38567584+zscott92@users.noreply.github.com>
2019-08-06 14:17:50 -0400

    module update_2

--- assets/javascript/calendar.js
+++ assets/javascript/calendar.js
@@ -1,86 +1,71 @@
-// var currentTime = new Date();
-// today = currentTime.toISOString();
-
-// var seventyTwoHours = new Date(currentTime.getTime() + 72 * 1000 * 60 * 60);
-// seventyTwoHours = seventyTwoHours.toISOString();
-
-// const clientId =
-//   "797197574459-upvhnomrje24c4ehlttj2u3u8e9gbfpb.apps.googleusercontent.com";
-// const apiKey = "AIzaSyCBcOGEbSoxbna7f9fWGKsQaz-D3vB3Oic";
-
-// var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-// const scope = "https://www.googleapis.com/auth/calendar.readonly";
-
-// function handleClientLoad() {
-//   gapi.client.setApiKey(apiKey);
-//   window.setTimeout(checkAuth, 1);
-// }
-
-// function checkAuth() {
-//   gapi.auth.authorize(
-//     { client_id: clientId, scope: scope, immediate: true },
-//     handleAuthResult
-//   );
-// }
-
-// function handleAuthResult(authResult) {
-//   var selectButton = document.getElementById("select-button");
-//   var resultPanel = document.getElementById("result-panel");
-//   var resultTitle = document.getElementById("result-title");
-
-//   if (authResult && !authResult.error) {
-//     selectButton.style.visibility = "hidden";
-//     resultPanel.className = resultPanel.className.replace(
-//       /(?:^|\s)panel-danger(?!\S)/g,
-//       ""
-//     );
-//     resultPanel.className += "panel-success";
-//     resultTitle.innerHTML = "Application Authorized";
-//     makeApiCall();
-//   } else {
-//     selectButton.style.visibility = "visible";
-//     resultPanel.className += " panel-danger";
-//     selectButton.onclick = handleAuthClick;
-//   }
-// }
-
-// function handleAuthClick(event) {
-//   gapi.auth.authorize(
-//     { apiKey: API_KEY, client_id: clientId, scope: scope, discoveryDocs: DISCOVERY_DOCS, immediate: false },
-//     handleAuthResult
-//   );
-//   return false;
-// }
-
-// var resource = {
-//   summary: "Sample Event " + Math.floor(Math.random() * 10 + 1),
-//   start: {
-//     dateTime: today
-//   },
-//   end: {
-//     dateTime: seventyTwoHours
-//   }
-// };
-
-// function makeApiCall() {
-//   gapi.client.load("calendar", "v3", function() {
-//     var request = gapi.client.calendar.events.insert({
-//       calendarId: "gk0pudanag1bhu35vkv5dunja4@group.calendar.google.com",
-//       resource: resource
-//     });
-
-//     // handle the response from our api call
-//     request.execute(function(resp) {
-//       if (resp.status == "confirmed") {
-//         document.getElementById("event-response").innerHTML =
-//           "Event created successfully. View it <a href='" +
-//           resp.htmlLink +
-//           "'>online here</a>.";
-//       } else {
-//         document.getElementById("event-response").innerHTML =
-//           "There was a problem. Reload page and try again.";
-//       }
-//       console.log(resp);
-//     });
-//   });
-// }
+$(document).ready(function() {
+		
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay'
+        },
+        defaultDate: '2016-12-12',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2016-12-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2016-12-07',
+                end: '2016-12-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2016-12-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2016-12-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2016-12-11',
+                end: '2016-12-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2016-12-12T10:30:00',
+                end: '2016-12-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2016-12-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2016-12-12T14:30:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2016-12-12T17:30:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2016-12-12T20:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2016-12-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'https://google.com/',
+                start: '2016-12-28'
+            }
+        ]
+    });
+    
+});
